8772d5229caeb55ce00b36cc8f35f1e2ee3e3219
Ipe92 <Ipe92@users.noreply.github.com>
2021-06-08 14:53:01 +0300

    ADD css inline

--- src/Content.js
+++ src/Content.js
@@ -5,7 +5,7 @@ const Content = (props) => {
         <main>
             <div id="card">
                 <h3>{props.title}</h3>
-                <img src={props.pic} alt={props.title}></img>
+                <img src={props.pic} alt={props.title} width="600px" height="400px"></img>
                 <li classname="content">Sisältö</li>
             </div>
         </main>
